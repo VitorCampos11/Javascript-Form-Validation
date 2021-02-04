7eee1c940f0f61bc956f2471826496e0d8e92732
Joao Vitor Felipe Campos <73002258+VitorCampos11@users.noreply.github.com>
2021-02-03 23:55:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 (function () {
   if (document.readyState === "complete") {
-    validateEmail();
-    validatePassword();
+    validate();
   }
 })();
 
-function validateEmail() {
-  const form = document.querySelector("#form");
-  const email = document.querySelector("#email");
+function validate() {
+  const form = document.getElementById("form");
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
   const pattern = /^[^ ]+@[a-z]+\.[a-z]{2,3}$/;
+  const patternPassword = /^(?=(?:.*?[A-Z]){3})(?=(?:.*?[0-9]){2})(?=(?:.*?[!@#$%*()_+^&}{:;?.]){1})(?!.*\s)[0-9a-zA-Z!@#$%;*(){}_+^&]*$/;
 
-  if (email.value.match(pattern)) {
+  if (email.match(pattern)) {
     form.classList.add("valid");
     form.classList.remove("invalid");
   } else {
     form.classList.remove("valid");
     form.classList.add("invalid");
   }
-}
 
+}
 function validatePassword() {
+  if (password.match(patternPassword)){
+    form.classList.add("validPassword");
+    form.classList.remove("invalidPassword");
+  }else {
+    form.classList.remove("validPassword");
+    form.classList.add("invalidPassword");
+  }
 }

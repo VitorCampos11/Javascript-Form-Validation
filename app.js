641ef8fad83c066517986b04b80bac4368cc742e
Roberto Rosa <roberto.rosa7@gmail.com>
2021-02-04 10:21:43 -0300

    convertendo projeto para javascript orientado a objeto

--- app.js
+++ app.js
@@ -1,23 +1,69 @@
-(function () {
-  if (document.readyState === "complete") {
-    validateEmail();
-    validatePassword();
+/*
+ * JAVASCRIPT PROCEDURAL - EXECUTA FUNÇOES DE ACORDO COM SUA DECLARÇÃO
+ */
+
+// (function () {
+//   if (document.readyState === "complete") {
+//     validateEmail();
+//     validatePassword();
+//   }
+// })();
+
+// function validateEmail() {
+//   const form = document.querySelector("#form");
+//   const email = document.querySelector("#email");
+//   const pattern = /^[^ ]+@[a-z]+\.[a-z]{2,3}$/;
+
+//   if (email.value.match(pattern)) {
+//     form.classList.add("valid");
+//     form.classList.remove("invalid");
+//   } else {
+//     form.classList.remove("valid");
+//     form.classList.add("invalid");
+//   }
+// }
+
+// function validatePassword() {
+// }
+
+/*
+ * JAVASCRIPT ORIENTADO A OBJETO - EXECUTA MÉTODO, PODENDO SER DECLARADO EM QUALQUER LUGAR DO SCRIPT
+ */
+class App {
+  constructor() {
+    this.app = {};
+    this.initialize();
+    this.initializeEvents();
+
+    // debug
+    console.log(this.app);
   }
-})();
 
-function validateEmail() {
-  const form = document.querySelector("#form");
-  const email = document.querySelector("#email");
-  const pattern = /^[^ ]+@[a-z]+\.[a-z]{2,3}$/;
+  // inicializar o app
+  initialize() {
+    // criar uma lista de id e coloca dentro da propriedade
+    document.querySelectorAll("[id]").forEach((element) => {
+      this.app[Format.formatToCamelCase(element.id)] = element;
+    });
+  }
 
-  if (email.value.match(pattern)) {
-    form.classList.add("valid");
-    form.classList.remove("invalid");
-  } else {
-    form.classList.remove("valid");
-    form.classList.add("invalid");
+  initializeEvents() {
+    const pattern = /^[^ ]+@[a-z]+\.[a-z]{2,3}$/;
+
+    this.app.email.addEventListener("keyup", (e) => {
+      console.log(e);
+    });
   }
 }
 
-function validatePassword() {
+class Format {
+  constructor() {}
+  // responsável por retorna uma lista de id
+  static formatToCamelCase(text) {
+    let div = document.createElement("div");
+    div.innerHTML = `<div data-${text}="id"></div>`;
+    return Object.keys(div.firstChild.dataset)[0];
+  }
 }
+
+window.app = new App();
